9afbcff554b28ad639b8f7144333b6f8e71e28a8
Mark J Menger <mark.j.menger@gmail.com>
2021-10-11 20:40:14 +0000

    parameterized bigiq license in DO declaration

--- test/fixtures/azure/postbuildconfig.tf
+++ test/fixtures/azure/postbuildconfig.tf
@@ -1,10 +1,23 @@
 module "postbuild-config-do" {
-  source  = "mjmenger/postbuild-config/bigip//do"
-  version = "0.3.3"
-  bigip_user       = module.bigip[0].f5_username
-  bigip_password   = module.bigip[0].bigip_password
-  bigip_address    = module.bigip[0].mgmtPublicIP
-  bigip_do_payload = templatefile("${path.module}/../../../do.json", { nameserver = var.nameserver })
+  count            = length(var.azs)
+  source           = "mjmenger/postbuild-config/bigip//do"
+  version          = "0.3.3"
+  bigip_user       = module.bigip[count.index].f5_username
+  bigip_password   = module.bigip[count.index].bigip_password
+  bigip_address    = module.bigip[count.index].mgmtPublicIP
+  bigip_do_payload = templatefile("${path.module}/../../../do.json", { 
+    nameserver     = var.nameserver
+    bigiq_hostname = "20.109.161.203"
+    bigiq_username = "admin"
+    bigiq_password = "3WL_lzySMoechDB1cp33e4s"
+    licensePool    = "F5-BIG-MSP-LOADV4-LIC",
+    skuKeyword1    = "F5-BIG-MSP-BR-5G",
+    unitOfMeasure  = "yearly",
+    reachable      = false,
+    bigIpUsername  = module.bigip[count.index].f5_username,
+    bigIpPassword  = module.bigip[count.index].bigip_password,
+    hypervisor     = "azure"
+    })
   depends_on = [
     module.bigip
   ]
